7df3e98d751eda294fa257fdf958872e849b65a3
Mike Orr <mike@predicate.us>
2018-04-20 21:09:07 -0400

    updates

--- combat_final.py
+++ combat_final.py
@@ -1,13 +1,18 @@
-# spin candidates
-# keep score (hits, survival time, health)
-# falling rocks
+# spin enemies
+# - keep score (hits, survival time, health)
+# - falling saw blades of death
 # wacky jumps
-# goals
-# auto crop actors?
+# - duration goal
 # throw something at zombies
 # have zombies fall out of the sky
 # golden star reward
-# make the piggy fall and die
+# - make the piggy fall and die (fun ways to die)
+# hostage
+# goal coins
+# levels
+# vary _health
+# complex obstacles
+# build and destroy to get a goal
 
 # a window 20 grid blocks wide by 15 grid height
 WIDTH = 20
@@ -36,9 +41,6 @@ for x in range(5):
 for x in range(2):
    image('crate1', pos=(x+6, 8), tag=OBSTACLE)
 
-#for x in range(6):
-#   image('crate1', pos=(x+5, 7))
-
 # create Porter, give him a mass so he falls down, use arrow keys to move left/right
 p = actor('Porter', pos=(17,0), size=2).mass(10).keys()
 
@@ -50,23 +52,41 @@ keydown('space', partial(p.jump, 7))
 
 # when an enemy hits the Porter
 def hit(enemy, porter):
+   # don't do anything when the enemy is dead
+   if enemy.health == 0:
+      return
+
    # enemy is moving right and Porter is punching from the left
    if enemy.direction == RIGHT and porter.action == ATTACK_LEFT:
       enemy.kill()
+      score(1, pos=LOWER_RIGHT)
    # enemy is moving left and Porter is punching from the right
    elif enemy.direction == LEFT and porter.action == ATTACK_RIGHT:
       enemy.kill()
+      score(1, pos=LOWER_RIGHT)
    # the enemy is alive but Porter didn't act. he dies!
-   elif enemy.health > 0:
+   else:
       porter.kill()
+      text('You survived {} seconds'.format(time()))
       gameover()
 
 # create an enemy from the left. move right.
+#def move_right(e):
+#   print('move_right')
+#   if e.pos[0] > WIDTH-2:
+#      e.destroy()
+#   else:
+#      e.move_to((WIDTH-2, e.pos[1]), animation=WALK_RIGHT)
+#      callback(partial(monitor, e, partial(move_right, e)), 0.75)
+
 def left():
-   e = actor('Zombie-8', pos=(-2,12.15), size=2).flip()
+   e = actor('Zombie-8', pos=(22,12.15), size=2).flip()
+   #e.mass(10)
    e.speed(1)
-   e.move_to((22, 12.15), animation=WALK_RIGHT, callback=e.destroy)
+   e.move_to((22, 12.15), animation=WALK_RIGHT)#.spin(0.3, 25)
    e.collides(p, hit)
+   #callback(partial(monitor, e, partial(move_right, e)), 0.75)
+callback(left, 6, FOREVER)
 
 # create an enemy from the right. move left.
 def right():
@@ -74,6 +94,7 @@ def right():
    e.speed(1)
    e.move_to((-2, 12.15), animation=WALK_LEFT, callback=e.destroy)
    e.collides(p, hit)
+callback(right, 6, FOREVER)
 
 # make saw blades fall
 def die(blade, porter):
@@ -90,11 +111,17 @@ def fall():
    b.speed(randint(1,6))
    b.move_to((pos[0], HEIGHT+5), callback=b.destroy)
    b.collides(p, die)
-
 callback(fall, randint(1, 5), FOREVER)
-# callback to create enemies
-callback(right, 6, FOREVER)
-callback(left, 6, FOREVER)
 
+def monitor(a, cb):
+   """ used by computer player (red and blue) to track progress and potentially replay movement """
+   if a.action.startswith(IDLE):
+      cb()
+   if not a.action.startswith(DIE):
+      callback(partial(monitor, a, cb), 0.5)
+
+# stopwatch
+stopwatch(prefix='Survival: ')
+score(prefix='Kills: ', pos=LOWER_RIGHT)
 # press 'r' key to restart game
 keydown('r', reset)
